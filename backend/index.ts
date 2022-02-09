5b500cb70de5ab78918bdcf7f15ccddc4f37ecd5
Rendhy 'k <rendika.debug@gmail.com>
2022-02-09 10:57:31 +0700

    fix validate

--- backend/index.ts
+++ backend/index.ts
@@ -3,32 +3,51 @@ import { Server, Socket } from "socket.io";
 
 const httpServer = createServer();
 const io = new Server(httpServer, {
-  // ...
+  // ...configure socket
 });
-import { MessageDto } from './dto/message.dto';
-// rest of the code remains same
+import { MessageDto } from "./dto/message.dto";
+/// declare default port
 const PORT = 8000;
 
-let welcome:MessageDto = {
-  name:'Bot', 
-  avatar:'https://cdn.quasar.dev/img/avatar1.jpg', 
-  message: 'Welcome to my paradise', 
-  stamp: '27 sec ago', 
-  sent: false, 
-  bgColor:'amber-7'
+/// default message on client connected
+let welcomeMessage: MessageDto = {
+  name: "Bot Chat",
+  avatar: "https://cdn.quasar.dev/img/avatar1.jpg",
+  message: "Welcome to my paradise",
+  stamp: "27 sec ago",
+  sent: false,
+  bgColor: "amber-7",
 };
 
-io.on('connection', (socket) => {
-  console.log('a user connected')
-  io.sockets.emit('welcome', welcome)
-  socket.on('chat_message', (msg) => {
-    console.log('message: ' + msg);
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  setTimeout(() => {
+    /// send message when user connected
+    io.sockets.emit("welcome", welcomeMessage);
+  }, 2000);
+  socket.on("chat_message", (data: MessageDto) => {
+    console.log("message: " + data);
+
+    io.sockets.emit("chat_message", data);
+
+    /// auto reply message if word contains string of hello
+    if (data.message.toLocaleLowerCase().includes("hello")) {
+      const message: MessageDto = {
+        name: "Bot Chat",
+        message: `Hello too ${data.name}`,
+        avatar: "https://cdn.quasar.dev/img/avatar1.jpg",
+        bgColor: "amber-7",
+        sent: false,
+        stamp: "1 sec ago",
+      };
+      io.sockets.emit("chat_message", message);
+    }
   });
 
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-  })
-})
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
 
 httpServer.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
--- frontend/src/pages/Index.vue
+++ frontend/src/pages/Index.vue
@@ -1,27 +1,35 @@
 <template>
   <q-page class="flex flex-center">
-    <div
-      style="width: 100%; max-width: 600px; height: 80vh; padding: 2em"
-      class="shadow-2 flex column justify-end"
-    >
-      <q-chat-message
-        name="me"
-        avatar="https://cdn.quasar.dev/img/avatar1.jpg"
-        :text="['hey, how are you?']"
-        stamp="7 minutes ago"
-        sent
-        bg-color="amber-7"
-      />
-      <q-chat-message
-        name="Jane"
-        avatar="https://cdn.quasar.dev/img/avatar5.jpg"
-        :text="['doing fine, how r you?']"
-        stamp="4 minutes ago"
-        text-color="white"
-        bg-color="primary"
-      />
-      <div class="q-pt-lg">
-        <q-input outlined v-model="message" :dense="true">
+    <div>
+      <div
+        style="
+          z-index: 0;
+          overflow: auto;
+          width: 70vw;
+          height: 80vh;
+          padding: 2em;
+        "
+        class="shadow-2"
+      >
+        <div v-for="(chat, index) in chatList" :key="index" class="q-mb-lg">
+          <q-chat-message
+            :name="chat.name"
+            :avatar="chat.avatar"
+            :text="[chat.message]"
+            :stamp="chat.stamp"
+            :sent="chat.sent"
+            :bg-color="chat.bgColor"
+          />
+        </div>
+      </div>
+      <div class="chat-text q-pt-lg">
+        <q-input
+          outlined
+          v-model="message"
+          :dense="true"
+          placeholder="Write message here"
+          @keydown.enter.prevent="sendMessage"
+        >
           <template v-slot:append>
             <q-icon
               v-if="message !== ''"
@@ -29,7 +37,7 @@
               @click="message = ''"
               class="cursor-pointer"
             />
-            <q-icon name="send" class="cursor-pointer" />
+            <q-icon name="send" class="cursor-pointer" @click="sendMessage" />
           </template>
         </q-input>
       </div>
@@ -39,19 +47,68 @@
 
 <script>
 import { io } from "socket.io-client";
-const socket = io('localhost:8000')
+const socket = io("localhost:8000");
 export default {
   name: "PageIndex",
   data() {
     return {
       message: null,
+      chatList: [],
+      username: null,
     };
   },
   created() {
+    this.chatList = [];
     socket.on("connected", () => {
-      console.log(socket.id)
-    })
+      console.log(socket.id);
+    });
+    socket.on("welcome", (data) => {
+      this.chatList.push(data);
+    });
+    socket.on("chat_message", (data) => {
+      this.chatList.push(data);
+    });
+    const getRandomString = (length) => {
+      var randomChars =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+      var result = "";
+      for (var i = 0; i < length; i++) {
+        result += randomChars.charAt(
+          Math.floor(Math.random() * randomChars.length)
+        );
+      }
+      return result;
+    };
+    /// set random username and random min length and max length
+    this.username = getRandomString(
+      Math.floor(Math.random() * (10 - 3 + 1)) + 3
+    );
+  },
+  methods: {
+    sendMessage() {
+      /// validate if null message
+      if (this.message) {
+        const message = {
+          name: this.username,
+          message: this.message,
+          avatar: "https://cdn.quasar.dev/img/avatar2.jpg",
+          stamp: "1 minute ago",
+          sent: true,
+          bgColor: "primary",
+        };
+        socket.emit("chat_message", message);
+        this.message = null;
+      }
+    },
   },
 };
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.chat-text {
+  position: absolute;
+  bottom: 0.5em;
+  right: 1em;
+  left: 1em;
+  z-index: 1;
+}
+</style>
